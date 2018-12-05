dcaab7a22adeeec476fb6e7581e80b631f5c0243
lucandiray <lucandiray@gmail.com>
2018-12-05 11:47:12 -0800

    added some more css and html

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $(.content).click(function() {
-
+    var y = 1
+    if (y = 1) {
+      $(this).html("<p>This is my first blog post</p>")
+      y += 1
+    }
+    else if (y = 2) {
+      $(this).html("<h1>farts</h1>")
+    }
   })
 });
